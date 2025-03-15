f222f8b8feec769f526b012b411552165a8d6ece
Khojiakbar2000 <ishonkulovkhojiakbar@gmail.com>
2025-03-15 14:07:13 -0700

    fix: modify routing system and controllers

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import path from 'path'
 import router from "./router"
+import routerAdmin from "./routerAdmin"
+
 
 /** 1-ENTRANCE **/
 const app = express();
@@ -14,6 +16,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 /** 4-ROUTERS **/
-app.use('/', router)
+app.use("/admin", routerAdmin)//EJS
+app.use("/", router) //REACT
 
 export default app;  
--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,9 @@
 import express from "express"
 const router = express.Router()
-import memberController from "./controllers/member.controller"
+import restaurantController from "./controllers/member.controller"
 
 
-router.get('/', memberController.goHome)
 
-router.get('/login',memberController.getLogin)
 
-    router.get('/signup ',memberController.getSignup)
 
 export default router;
