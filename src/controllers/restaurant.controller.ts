cfb64324d4317c51c744dbf092172acd34ac6958
Khojiakbar2000 <ishonkulovkhojiakbar@gmail.com>
2025-03-25 20:19:42 -0700

    login and signup processes

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -1,88 +1,73 @@
-import {Request,Response} from "express"
-import {T} from "../libs/types/common"
-import MemberService from "../models/Member.service"
-import { LoginInput, MemberInput } from "../libs/types/member";
+import { Request, Response } from "express"; 
+import { T } from "../libs/types/common"; 
+import MemberService from "../models/Member.service"; 
+import { MemberInput, LoginInput } from "../libs/types/member"; 
+
+
 import { MemberType } from "../libs/enums/member.enum";
 
+const memberService = new MemberService();
+
 const restaurantController: T = {};
-restaurantController.goHome = (req: Request, res:Response) =>{
-    try{
-        console.log(`goHome`)
-    res.send("HomePage")
-}catch(err){
-console.log("Error, goHome:",err)
-}
+restaurantController.goHome = (req: Request, res: Response) => {
+  try {
+    console.log("gohome");
+    res.send("Homepage");
+  } catch (err) {
+    console.log("Error goHome", err);
+  }
+};
 
+restaurantController.getSignup = (req: Request, res: Response) => {
+  try {
+    res.send("Signup page"); 
+  } catch (err) {
+    res.send(err);
+  }
+};
 
-}
+restaurantController.getLogin = (req: Request, res: Response) => {
+  try {
+    res.send("Login page");
+  } catch (err) {
+    console.log("Error getLogin", err);
+  }
+};
 
+restaurantController.processSignup = async (req: Request, res: Response) => {
+  try {
+    console.log("processSignup");
+   
 
-restaurantController.getLogin = (req: Request, res:Response) =>{
-    try{
-        console.log(`getLogin`)
-    res.send("Login Page")
-}catch(err){
-console.log("Error, getLogin:",err)
-}
+    
 
+    const newMember: MemberInput = req.body; 
+    newMember.memberType = MemberType.RESTAURANT;
 
-}
+    const result = await memberService.processSignup(newMember);
+   //TODO: SESSIONS: AUTHENTICATION
 
-restaurantController.getSignup = (req: Request, res:Response) =>{
-    try{
-        console.log(`getSignup`)
-    res.send("Signup Page")
-}
-catch(err){
-console.log("Error, getSignup:",err)
-}
+    res.send(result);
+  } catch (err) {
+    console.log("Error processSignup", err);
+    res.send(err);
+  }
+};
 
+restaurantController.processLogin = async (req: Request, res: Response) => {
+  try {
+    console.log("processLogin");
+    console.log("body:", req.body);
+    const input: LoginInput = req.body;
 
-}
+    const result = await memberService.processLogin(input);
+    //TODO: SESSIONS: AUTHENTICATION
 
-restaurantController.processLogin = async(req: Request, res:Response) =>{
-    try{
-        console.log(`processLogin`)
-        console.log("body:", req.body)
-        const input : LoginInput= req.body;
-
-        const memberService = new MemberService();
-        const result = await memberService.processLogin(input);
-
-        res.send(result)
-}
-catch(err){
-console.log("Error, processLogin:",err);
-res.send(err);
-}
-
-
-}
-
-restaurantController.processSignup = async(req: Request, res:Response) =>{
-    try{
-        console.log("processSignup")
-        console.log("body", req.body)
-        
-
-        const newMember: MemberInput = req.body;
-        newMember.memberType = MemberType.RESTAURANT;
-        
-        const memberService = new MemberService();
-        const result = await memberService.processSignup(newMember);
-       
-        res.send(result)
-}
-catch(err){
-console.log("Error, processSignup:",err)
-res.send (err)
-}
-
-
-}
-
-
-
-
+    res.send(result);
+  } catch (err) {
+    console.log("Error processLogin", err);
+    res.send(err);
+  }
+};
 
 export default restaurantController;
--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,9 @@
-import express from "express"
-const router = express.Router()
-import restaurantController from "./controllers/member.controller"
+import express from "express";
+const router = express.Router();
+import memberController from "./controllers/member.controller";
 
-
-
+router.post("/login", memberController.login);
+router.post("/signup", memberController.signup);
 
 
 export default router;
