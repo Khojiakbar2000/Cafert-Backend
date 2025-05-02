1c0ac43fd0b270d998c2e34c61b0feaab6109217
Khojiakbar2000 <ishonkulovkhojiakbar@gmail.com>
2025-05-01 18:23:52 -0700

    fix: getMemberDetail business logic

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -71,6 +71,22 @@ memberController.logout = (req: ExtendedRequest, res: Response) =>{
   }
 }
 
+
+
+memberController.getMemberDetail = async (req: ExtendedRequest, res: Response) =>{
+  try{
+    console.log("getMemberDetail");
+    const result = await memberService.getMemberDetail(req.member);
+    
+    res.status(HttpCode.OK).json(result)
+
+  }catch(err){
+    console.log("Error, getMemberDetail", err);
+    if(err instanceof Errors) res.status(err.code).json(err)
+      else res.status(Errors.standard.code).json(Errors.standard);
+  }
+}
+
 memberController.verifyAuth = async (
   req:ExtendedRequest, 
   res: Response,
@@ -102,7 +118,7 @@ memberController.retrieveAuth = async (
        next();
       
   }catch(err){
-    console.log("Error, retrieveAuth", err);
+    console.log("Error, retrieveAuth:", err);
     next();
   }
 }
