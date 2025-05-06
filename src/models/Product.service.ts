11ac9c29b773f62949555b8b4d423d61481292d9
Khojiakbar2000 <ishonkulovkhojiakbar@gmail.com>
2025-05-06 01:17:23 -0700

    feat: develop getProduct rest Api

--- src/models/Product.service.ts
+++ src/models/Product.service.ts
@@ -6,6 +6,7 @@ import { Message } from "../libs/Errors";
 import { shapeIntoMongooseObjectId } from "../libs/config";
 import { ProductStatus } from "../libs/enums/product.enum";
 import { T } from "../libs/types/common";
+import {ObjectId} from "mongoose"
 
 
 
@@ -22,23 +23,38 @@ constructor(){
 public async getProducts(inquiry: ProductInquiry): Promise<Product[]>{
 const match: T = {productStatus: ProductStatus.PROCESS}
 
-if(inquiry.productCollection) match.productCollection = inquiry.productCollection;
+if(inquiry.productCollection)
+ match.productCollection = inquiry.productCollection;
 if(inquiry.search) {
-    match.productName = {$regex: new RegExp(inquiry.search, "i")}
+    match.productName = {$regex: new RegExp(inquiry.search, "i")};
 }
 
-const sort: T = inquiry.order === "productPrice" 
+const sort: T = 
+inquiry.order === "productPrice" 
 ? {[inquiry.order]: 1} 
 : {[inquiry.order]: -1}
 
-const result = await this.productModel.aggregate([
+const result = await this.productModel
+.aggregate([
 {$match: match},
 {$sort: sort},
  {$skip: (inquiry.page * 1 - 1)*inquiry.limit },
  {$limit: inquiry.limit *1 },
-]).exec()
+])
+.exec()
 if(!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
 return result;
+}
+
+public async getProduct(memberId: ObjectId | null, id: string): Promise<Product>{
+const productId = shapeIntoMongooseObjectId(id);
+
+let result = await this.productModel.findOne({_id: productId, productStatus: ProductStatus.PROCESS})
+.exec();
+if(!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+
+//TODO: if authenticated users => first => view log creation
+return result
 }
 
 /** SSR */
